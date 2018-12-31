99d0daabf8aa8e28c8ebec5a5b9459214c42d9d1
Derrick Timmermans <derrick.timmermans@outlook.com>
2018-12-31 12:04:43 +0800

    Implement Teleport on both sides
    This was a triumph.
    I'm making a note here: HUGE SUCCESS.
    It's hard to overstate my satisfaction..🎵🎵

--- Test Bench/Header Files/Systems/PlayerMovement.h
+++ Test Bench/Header Files/Systems/PlayerMovement.h
@@ -20,18 +20,23 @@ public:
 				while (potentialDistance > 0) {
 					bool moved = false;
 
-					if (position.x % 8 == 0 && position.y%8 ==0) {
+					if (position.x % 8 == 0 && position.y % 8 == 0 && player.moveX == 0 && player.moveY ==0) {
 						player.direction = player.nextDir;
 						auto& AnimSet = m_registry.get<AnimationSet>(entity);
 						switch (player.direction) {
 						case right:
 							AnimSet.changeAnimation("right");
-							if (valid(layout[position.y / 8][(position.x / 8) + 1]))
+							if (position.x / 8 >= 27) {
+								player.moveX = +16;
+							}
+							else	if (valid(layout[position.y / 8][(position.x / 8) + 1]))
 								player.moveX += 8;
 							break;
 						case left:
 							AnimSet.changeAnimation("left");
-							if (valid(layout[position.y / 8][(position.x / 8) -1]))
+							if (position.x / 8 == 0) {
+								player.moveX = -16;
+							}else if (valid(layout[position.y / 8][(position.x / 8) - 1]))
 								player.moveX -= 8;
 							break;
 						case up:
@@ -49,10 +54,17 @@ public:
 
 					if (player.moveX > 0) {
 						position.x += 1;
+						if (position.x == 28 * 8) {
+							position.x = -8;
+						}
 						--potentialDistance;
 						--player.moveX;
-					}else if (player.moveX < 0) {
+					}
+					else if (player.moveX < 0) {
 						position.x -= 1;
+						if (position.x == -8) {
+							position.x = 28*8;
+						}
 						--potentialDistance;
 						++player.moveX;
 					}
