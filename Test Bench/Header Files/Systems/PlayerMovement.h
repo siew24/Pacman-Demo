c9248685a98d0edf5240f43d8be7ef5dc39621f6
Derrick Timmermans <derrick.timmermans@outlook.com>
2018-12-31 12:32:05 +0800

    Factor in delta time in movement calculation
    Now everybody will have equal movement speed regardless of frame rate.

--- Test Bench/Header Files/Systems/PlayerMovement.h
+++ Test Bench/Header Files/Systems/PlayerMovement.h
@@ -15,28 +15,35 @@ public:
 	virtual void update(std::optional<double> deltaTime = std::nullopt) override {
 		m_registry.view<Pacman, Position>().each(
 			[&](auto entity, Pacman& player, Position& position) {
-				int potentialDistance = 1;
+				if (deltaTime.value() < 500.0)
+					lastUpdate += deltaTime.value() / 1000;
+				const double speed = 11.0;
+				const double unitTime = 11.0 / 1000.0;
+				int potentialDistance = 0;
+				if (lastUpdate > unitTime) {
+					potentialDistance = static_cast<int>(lastUpdate / unitTime);
+					lastUpdate = std::fmod(lastUpdate, unitTime);
+				}
+
 
 				while (potentialDistance > 0) {
-					bool moved = false;
-
-					if (position.x % 8 == 0 && position.y % 8 == 0 && player.moveX == 0 && player.moveY ==0) {
+					bool moved = 0;
+					if (position.x % 8 == 0 && position.y % 8 == 0 && player.moveX == 0 && player.moveY == 0) {
 						player.direction = player.nextDir;
 						auto& AnimSet = m_registry.get<AnimationSet>(entity);
 						switch (player.direction) {
 						case right:
 							AnimSet.changeAnimation("right");
-							if (position.x / 8 >= 27) {
+							if (position.x / 8 >= 27)
 								player.moveX = +16;
-							}
-							else	if (valid(layout[position.y / 8][(position.x / 8) + 1]))
+							else if (valid(layout[position.y / 8][(position.x / 8) + 1]))
 								player.moveX += 8;
 							break;
 						case left:
 							AnimSet.changeAnimation("left");
-							if (position.x / 8 == 0) {
+							if (position.x / 8 == 0)
 								player.moveX = -16;
-							}else if (valid(layout[position.y / 8][(position.x / 8) - 1]))
+							else if (valid(layout[position.y / 8][(position.x / 8) - 1]))
 								player.moveX -= 8;
 							break;
 						case up:
@@ -59,34 +66,37 @@ public:
 						}
 						--potentialDistance;
 						--player.moveX;
+						moved = true;
 					}
 					else if (player.moveX < 0) {
 						position.x -= 1;
 						if (position.x == -8) {
-							position.x = 28*8;
+							position.x = 28 * 8;
 						}
 						--potentialDistance;
 						++player.moveX;
+						moved = true;
 					}
 					else if (player.moveY < 0) {
 						position.y -= 1;
 						--potentialDistance;
 						++player.moveY;
+						moved = true;
 					}
 					else if (player.moveY > 0) {
 						position.y += 1;
 						--potentialDistance;
 						--player.moveY;
+						moved = true;
 					}
-
 
 					if (!moved)
 						break;
 				}
-
 			}
 		);
 	};
 
 	std::vector<std::vector<int>> layout;
+	double lastUpdate = 0;
 };
