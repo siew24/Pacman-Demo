fbb1fc7ed6979b64c0b90b672de2b87813d343cd
Derrick Timmermans <derrick.timmermans@outlook.com>
2019-03-30 11:59:15 +0800

    Remove BF

--- BloomFramework/include/Components/AnimationSet.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Graphics/AnimationSet.h"
-
-namespace bloom::components {
-	using AnimationSet = bloom::graphics::AnimationSet;
-}
--- BloomFramework/include/Components/Sprite.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Graphics/Sprite.h"
-
-namespace bloom::components {
-	using Sprite = bloom::graphics::Sprite;
-}
