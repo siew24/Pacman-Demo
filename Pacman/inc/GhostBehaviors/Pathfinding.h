fb7842a547da5e103ba143703bfae4a400ce898d
Derrick Timmermans <derrick.timmermans@outlook.com>
2019-04-06 19:52:27 +0800

    Improve ghost pathfinding conflict resolution
    Now when two tiles have equal distance to the target tile, the highest priority direction is preferred. (up, left, down, right)

--- Pacman/inc/GhostBehaviors/Pathfinding.h
+++ Pacman/inc/GhostBehaviors/Pathfinding.h
@@ -10,7 +10,7 @@ using namespace bloom::components;
 namespace ghostBehaviors {
 	auto generateCandidates(const Tile& target, const Tile& currentTile, Ghost& ghost, std::vector<std::vector<int>>& layout) {
 		std::vector<std::pair<Direction, double>> posibilities{};
-		if (Tile candidate{ currentTile.x, currentTile.y - 1 }; (layout[candidate.y][candidate.x] == 0|| (std::abs(layout[candidate.y][candidate.x])&256) == 256) && (std::abs(layout[currentTile.y][currentTile.x]) & 256) != 256 && ghost.direction != Direction::down) {
+		if (Tile candidate{ currentTile.x, currentTile.y - 1 }; (layout[candidate.y][candidate.x] == 0 || (std::abs(layout[candidate.y][candidate.x]) & 256) == 256) && (std::abs(layout[currentTile.y][currentTile.x]) & 256) != 256 && ghost.direction != Direction::down) {
 			int xDist = std::abs(candidate.x - target.x), yDist = std::abs(candidate.y - target.y);
 			double distance = std::sqrt(std::pow(xDist, 2) + std::pow(yDist, 2));
 			posibilities.emplace_back(std::make_pair(Direction::up, distance));
@@ -23,7 +23,7 @@ namespace ghostBehaviors {
 		if (Tile candidate{ currentTile.x - 1, currentTile.y }; (currentTile.x == 0 || layout[candidate.y][candidate.x] == 0 || (std::abs(layout[candidate.y][candidate.x]) & 256) == 256) && ghost.direction != Direction::right) {
 			int xDist = std::abs(candidate.x - target.x), yDist = std::abs(candidate.y - target.y);
 			double distance = std::sqrt(std::pow(xDist, 2) + std::pow(yDist, 2));
-			posibilities.emplace_back(std::make_pair(Direction::left , distance));
+			posibilities.emplace_back(std::make_pair(Direction::left, distance));
 		}
 		if (Tile candidate{ currentTile.x + 1, currentTile.y }; (currentTile.x >= 27 || layout[candidate.y][candidate.x] == 0 || (std::abs(layout[candidate.y][candidate.x]) & 256) == 256) && ghost.direction != Direction::left) {
 			int xDist = std::abs(candidate.x - target.x), yDist = std::abs(candidate.y - target.y);
@@ -31,19 +31,22 @@ namespace ghostBehaviors {
 			posibilities.emplace_back(std::make_pair(Direction::right, distance));
 		}
 
-		if (Tile candidate{ currentTile.x, currentTile.y - 1 };(candidate.y!=0) && ((layout[candidate.y][candidate.x] == 39||layout[candidate.y-1][candidate.x] == 39) && ghost.released && (ghost.currentMode == BehaviourModes::chase || ghost.currentMode == BehaviourModes::scatter))) {
+		if (Tile candidate{ currentTile.x, currentTile.y - 1 }; (candidate.y != 0) && ((layout[candidate.y][candidate.x] == 39 || layout[candidate.y - 1][candidate.x] == 39) && ghost.released && (ghost.currentMode == BehaviourModes::chase || ghost.currentMode == BehaviourModes::scatter))) {
 			posibilities.clear();
 			posibilities.emplace_back(std::make_pair(Direction::up, 0.0));
 			ghost.inHouse = false;
 		}
 
-		if (Tile candidate{ currentTile.x, currentTile.y + 1 }; (candidate.y != layout.size()-1) && ((layout[candidate.y][candidate.x] == 39 || layout[candidate.y - 1][candidate.x] == 39) && ghost.currentMode == BehaviourModes::dead)) {
+		if (Tile candidate{ currentTile.x, currentTile.y + 1 }; (candidate.y != layout.size() - 1) && ((layout[candidate.y][candidate.x] == 39 || layout[candidate.y - 1][candidate.x] == 39) && ghost.currentMode == BehaviourModes::dead)) {
 			posibilities.clear();
 			posibilities.emplace_back(std::make_pair(Direction::down, 0.0));
 			ghost.inHouse = true;
 		}
 
-		std::sort(posibilities.begin(), posibilities.end(), [](auto& lhs, auto& rhs) {return lhs.second < rhs.second; });
+		std::sort(posibilities.begin(), posibilities.end(), [](auto & lhs, auto & rhs) {
+			return (lhs.second < rhs.second) 
+				|| (lhs.second == rhs.second && lhs.first < rhs.first);
+			});
 		return posibilities;
 	}
 }
